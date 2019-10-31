95eea40d3140b93dc20f429e63ab63c9d743b358
andrew.jin.19 <sbjin2004@gmail.com>
2019-10-31 18:51:32 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+import picamera
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+app._static_folder = "home/pi"
+
+@app.route("/pictures")
+def hello()
+    with picamera.PiCamera() as camera:
+    camera.capture('image.jpg')
+    return app.send_static_file('image.jpg')
+
+@app.route("/interface")
+def interface_display(n):
+	return render_template("interface.html")
+
+#app = Flask(__name__)
+
+#@app.route('/photo')
+#def hello_world():
+    #return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
